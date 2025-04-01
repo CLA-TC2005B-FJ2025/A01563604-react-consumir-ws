95b1889ffb8178d3b7e326aa0dfc03f1bbee19fa
Jared Marquez <142861359+JaredMarquez@users.noreply.github.com>
2025-04-01 16:24:20 +0000

    commit

--- src/api.js
+++ src/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const getUsuario = async (user_id) => {
 
-    const response = await axios.get('http://localhost:2025/test/' + user_id, {
-    });
+    const response = await axios.get('https://ubiquitous-orbit-wr7xrqxv9wgxf9wrp-2025.app.github.dev/test/' + user_id, {});
     
     console.log(response)
     return response;
